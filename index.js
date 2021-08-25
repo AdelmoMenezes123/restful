89a81730100aef6b5c5d4a0d604f6e7b109fed3d
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-08-25 13:17:41 -0300

    add banco NeDB e cria rota de cadastrar usuario

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //incluir todas as rotas que esta na pasta routes, dentro de app
 consign().include('routes').into(app);
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+
 module.exports = (app) => {
     app.get('/users', (req, res) => {
         res.statusCode = 200;
@@ -15,5 +21,19 @@ module.exports = (app) => {
                 id: 2
             }]
         });
+    });
+
+    app.post('/users', (req, res) => {
+        db.insert(req.body, (err, user) => {
+            if (err) {
+                console.log(`Error: ${err}`)
+                res.status(400).json({
+                    error: err
+                });
+            }else{
+                res.status(200).json(user)
+            }
+
+        })
     });
 }
